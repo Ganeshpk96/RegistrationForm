4879061937aad4a33f250963d8fc74b8aa195d5c
Ganeshpk96 <ganeshpk96@gmail.com>
2022-12-20 23:31:10 +0530

    List Added

---
+++ List.js
@@ -0,0 +1,6 @@
+var item = document.getElementsByClassName('list-group-item');
+console.log(item);
+
+item[1].style.fontWeight = "bold";
+item[2].style.fontWeight = "bold";
+item[2].style.color = "green";
