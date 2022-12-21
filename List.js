d8ff725fa33a3e2d237bbd3bb98a795a7344a68c
Ganeshpk96 <ganeshpk96@gmail.com>
2022-12-21 18:43:22 +0530

    GetElementbyClassname

--- List.js
+++ List.js
@@ -1,6 +1,11 @@
 var item = document.getElementsByClassName('list-group-item');
 console.log(item);
 
+item[0].style.fontWeight = "bold";
 item[1].style.fontWeight = "bold";
 item[2].style.fontWeight = "bold";
-item[2].style.color = "green";
+item[2].style.color = "white";
+item[2].style.backgroundColor = "green";
+item[3].style.fontWeight = "bold";
+item[4].style.fontWeight = "bold";
+
