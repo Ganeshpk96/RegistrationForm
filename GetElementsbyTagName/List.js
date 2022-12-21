b1c37ed1c63a79779bca9c318eba90f002c182be
Ganeshpk96 <ganeshpk96@gmail.com>
2022-12-21 19:03:30 +0530

    GetElementsByTagname

---
+++ GetElementsbyTagName/List.js
@@ -0,0 +1,26 @@
+// var item = document.getElementsByClassName('list-group-item');
+// console.log(item);
+
+// item[0].style.fontWeight = "bold";
+// item[1].style.fontWeight = "bold";
+// item[2].style.fontWeight = "bold";
+// item[2].style.color = "white";
+// item[2].style.backgroundColor = "green";
+// item[3].style.fontWeight = "bold";
+// item[4].style.fontWeight = "bold";
+
+var li = document.getElementsByTagName("li");
+console.log(li);
+
+li[0].style.fontWeight = "bold";
+li[1].style.fontWeight = "bold";
+li[2].style.fontWeight = "bold";
+li[2].style.color = "white";
+li[2].style.backgroundColor = "green";
+li[3].style.fontWeight = "bold";
+li[4].style.fontWeight = "bold";
+li[5].style.fontWeight = "bold";
+li[5].style.listStyle = "none";
+li[5].style.padding = "10px";
+li[5].style.border = "2px solid #ccc";
+li[5].style.backgroundColor = "grey";
